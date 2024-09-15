f497a11b19ce0f5effe40e864bfe796b10c8d19c
David Tang <687449+iamdtang@users.noreply.github.com>
2024-09-14 21:43:25 -0700

    Demo 2: Fetching Pokemons with $.ajax
    npm i jquery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import "./App.css";
+import { useState } from "react";
+import $ from "jquery";
+
+export function fetchPokemons() {
+  return $.ajax({
+    type: "GET",
+    url: "https://pokeapi.co/api/v2/pokemon?limit=100",
+  });
+}
 
 function App() {
-  const groceries = ["Broccoli", "Kale", "Sweet potatoes"];
+  const [pokemons, setPokemons] = useState([]);
 
   return (
-    <ul>
-      {groceries.map((groceryItem) => {
-        return <li key={groceryItem}>{groceryItem}</li>;
-      })}
-    </ul>
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          fetchPokemons().then((response) => {
+            setPokemons(response.results);
+          });
+        }}
+      >
+        Show Pokemon
+      </button>
+
+      {pokemons.length > 0 && (
+        <ul>
+          {pokemons.map((pokemon) => {
+            return <li key={pokemon.name}>{pokemon.name}</li>;
+          })}
+        </ul>
+      )}
+    </div>
   );
 }
 
